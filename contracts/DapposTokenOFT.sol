70cf73a68d136e636bbcf77656f19304fee47bd1
Andy <1363708613@qq.com>
2025-10-24 17:31:25 +0800

    feat: add pause to oft

--- contracts/DapposTokenOFT.sol
+++ contracts/DapposTokenOFT.sol
@@ -2,13 +2,30 @@
 pragma solidity ^0.8.22;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {OFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
 
-contract DapposTokenOFT is OFT {
+contract DapposTokenOFT is OFT, Pausable {
     constructor(
         string memory _name,
         string memory _symbol,
         address _lzEndpoint,
         address _owner
     ) OFT(_name, _symbol, _lzEndpoint, _owner) Ownable(_owner) {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override whenNotPaused {
+        super._update(from, to, value);
+    }
 }

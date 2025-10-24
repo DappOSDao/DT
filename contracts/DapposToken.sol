7476bdedcf88e8ad0ad0eeb8943e3d278ca0381b
Andy <1363708613@qq.com>
2025-10-24 17:42:47 +0800

    feat: delete ERC20Permit

--- contracts/DapposToken.sol
+++ contracts/DapposToken.sol
@@ -4,15 +4,14 @@ pragma solidity ^0.8.22;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DapposToken is ERC20, ERC20Pausable, Ownable, ERC20Permit {
+contract DapposToken is ERC20, ERC20Pausable, Ownable {
     constructor(
         address _owner,
         address _treasury,
         uint256 _totalSupply
-    ) ERC20("DAPPOS", "DOS") Ownable(_owner) ERC20Permit("DAPPOS") {
+    ) ERC20("DAPPOS", "DOS") Ownable(_owner) {
         require(_treasury != address(0), "Treasury cannot be zero address");
         require(_totalSupply > 0, "Total supply must be greater than zero");
 
